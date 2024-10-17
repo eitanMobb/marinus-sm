14807c0c4048d0c5c85625166a6e9633c25a7976
Mobb autofixer <git@mobb.ai>
2024-10-17 14:50:37 +0000

    mobb fix commit: 2bb82d5f-691d-4790-afcf-c87bad19b5d7

--- web_server/config/models/cidr_graphs.js
+++ web_server/config/models/cidr_graphs.js
@@ -12,6 +12,7 @@
  * governing permissions and limitations under the License.
  */
 
+const mongoSanitize = require('express-mongo-sanitize');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -42,7 +43,7 @@ module.exports = {
     getCIDRGraphConfigByZone: function (zone) {
         let limitQuery = { 'config': 1 };
         return cidrGraphModel.findOne({
-            'zone': zone,
+            'zone': mongoSanitize.sanitize({ data: zone }).data,
         }, limitQuery).exec();
     },
     getCIDRGraphLinksByZone: function (zone) {
